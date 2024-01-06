7ba33f04d13674c94548c741af6995ada09bc0a2
香芋派大星 <96895927+chocookie7@users.noreply.github.com>
2024-01-06 18:24:44 +0800

    Add files via upload

---
+++ class1/class1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace class1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textName3 = new System.Windows.Forms.TextBox();
+            this.textName2 = new System.Windows.Forms.TextBox();
+            this.textName1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(658, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "姓名";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(262, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "学号";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(461, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "专业";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textName3
+            // 
+            this.textName3.Location = new System.Drawing.Point(504, 41);
+            this.textName3.Name = "textName3";
+            this.textName3.Size = new System.Drawing.Size(100, 25);
+            this.textName3.TabIndex = 4;
+            // 
+            // textName2
+            // 
+            this.textName2.Location = new System.Drawing.Point(323, 41);
+            this.textName2.Name = "textName2";
+            this.textName2.Size = new System.Drawing.Size(100, 25);
+            this.textName2.TabIndex = 5;
+            // 
+            // textName1
+            // 
+            this.textName1.Location = new System.Drawing.Point(141, 41);
+            this.textName1.Name = "textName1";
+            this.textName1.Size = new System.Drawing.Size(100, 25);
+            this.textName1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(83, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(650, 319);
+            this.label4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textName1);
+            this.Controls.Add(this.textName2);
+            this.Controls.Add(this.textName3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textName3;
+        private System.Windows.Forms.TextBox textName2;
+        private System.Windows.Forms.TextBox textName1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
