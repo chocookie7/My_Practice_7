681ec0ab000e9df37ea6dc23a3eab13679b63cbc
香芋派大星 <96895927+chocookie7@users.noreply.github.com>
2024-01-06 18:28:00 +0800

    Add files via upload

---
+++ class5.2/class5.2/FindNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class5._2
+{
+    internal class FindNumber
+    {
+        public int GetMax(int num1,int num2)
+        {
+            return num1 > num2?num1:num2;
+        }
+        public int GetMax(int num1,int num2,int num3)
+        {
+            int Max = num1;
+            if (Max < num2) { Max = num2; }
+            if (Max < num3) { Max = num3; }
+            return Max;
+        }
+    }
+}
---
+++ class5.2/class5.2/Form1.cs
@@ -0,0 +1,24 @@
+namespace class5._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1=Convert.ToInt32(textBox1.Text);
+            int num2=Convert.ToInt32(textBox2.Text);    
+            int num3=Convert.ToInt32(textBox3.Text);
+            FindNumber fmax = new FindNumber();
+            label4.Text +="\n"
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
